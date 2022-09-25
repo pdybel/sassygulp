c517449eedc7b2527e1bf3518bada25f70ddca36
Pablo Dybel <pablodybel@gmail.com>
2022-09-25 17:20:01 -0300

    imagenes1

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+const { src, dest, watch, parallel } = require("gulp");
+
+// CSS
+const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
+
+
+// imagenes   
+const cache= require('gulp-cache');
+const imagemin = require("gulp-imagemin");
+const webp =require('gulp-webp');
+const avif = require('gulp-avif');  /*para instalar la dependencia es npm i --save-dev gulp-avif    y asi con todas cambiar el gulp-(poner la dependencia) */
+
+function css(done) {
+    src("src/scss/**/*.scss") //identificar el archivo sass
+        .pipe(plumber()) //para no frenar por cada error
+        .pipe(sass()) //compilarlo
+        .pipe(dest("build/css")) //almacenorla en el disco duro
+             
+    done(); //callback que avisa a gulp cuando llegamos al final
+}
+
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'))
+
+
+    done()
+}
+
+
+function versionWebp ( done ) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')  /*busca los dos formatos */
+        .pipe(webp(opciones) )
+        .pipe (dest('build/img'))
+    done()
+}
+
+function versionAvif ( done ) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')  /*busca los dos formatos */
+        .pipe(avif(opciones) )
+        .pipe (dest('build/img'))
+    done()
+}
+
+
+function dev(done) {
+    watch("src/scss/**/*.scss", css)
+    done();
+}
+
+
+exports.css = css;
+exports.imagenes= imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
